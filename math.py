ac758bb17e18461ad9e278bf0ed93099eac819d0
Yiyun Qin <yiyun.qin@mths.ca>
2022-02-25 19:23:36 +0000

    circle math

---
+++ math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Yiyun Qin
+# Created on February 2022
+# This is the math program, with proper style
+
+
+def main():
+    import math
+
+    print("If a circle has a radius of 15 cm:")
+    print("")
+    print("Area is {} cm².".format(math.pi * pow(15, 2)))
+    print("Perimetre is {} cm.".format(2 * (math.pi * 15)))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
