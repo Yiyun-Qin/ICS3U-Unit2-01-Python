10920c3c5e1bca7630a28ef0ec8a75b32ed74e96
Yiyun Qin <yiyun.qin@mths.ca>
2022-02-25 19:26:12 +0000

    circle math

--- math.py
+++ math.py
@@ -8,10 +8,10 @@
 def main():
     import math
 
-    print("If a circle has a radius of 15 cm:")
+    print("If a circle has a radius of 15 mm:")
     print("")
-    print("Area is {} cm².".format(math.pi * pow(15, 2)))
-    print("Perimetre is {} cm.".format(2 * (math.pi * 15)))
+    print("Area is {} mm².".format(math.pi * pow(15, 2)))
+    print("Perimetre is {} mm.".format(2 * (math.pi * 15)))
     print("")
     print("Done.")
 
